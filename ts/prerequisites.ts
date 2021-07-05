a87438d6c48c18bd53620f869465fccde5b361f4
MoatShrimp <mathias.viklund@gmail.com>
2021-07-05 22:48:52 +0200

    Experimenting with making a fluid API for general DOM management

--- ts/prerequisites.ts
+++ ts/prerequisites.ts
@@ -4,6 +4,7 @@ declare var YAML: any; //yaml.js 0.3.0, github.com/jeremyfa/yaml.js/
 declare var saveAs: any; //FileSaver v2.0.5, github.com/eligrey/FileSaver.js/
 //JSZip for zipping batches of sprites
 declare var JSZip: any; //JSZip v3.6.0, github.com/Stuk/jszip/
+declare var fflate: any; //JSZip v3.6.0, github.com/Stuk/jszip/
 
 //SpriteMeta type
 interface SpriteMeta {
@@ -17,4 +18,15 @@ interface SpriteMeta {
 //getElementById shorthand
 const byId:any = document.getElementById.bind(document);
 const byClass:any = document.getElementsByClassName.bind(document);
+
+function ofId(name) {
+
+    function func() {
+        return document.getElementById(name);
+    }
+
+    return func;
+}
+
+const adam = (name => ofId(name));
 
